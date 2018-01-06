241e56fe5c52e80fa1c6f36524d09f6cc060ac53
= <=>
2018-01-06 19:13:56 +0100

    Added some documentation to the tests

--- tests/test_psflib_byte.py
+++ tests/test_psflib_byte.py
@@ -1,3 +1,27 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2018 by Karsten Lehmann <ka.lehmann@yahoo.com>
+
+#	This file is part of PySFedit.
+#
+#	PySFedit is free software: you can redistribute it and/or modify
+#	it under the terms of the GNU General Public License as published by
+#	the Free Software Foundation, either version 3 of the License, or
+# 	(at your option) any later version.
+#
+#	PySFedit is distributed in the hope that it will be useful,
+#	but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#	GNU General Public License for more details.
+#
+# 	You should have received a copy of the GNU General Public License
+#	long with PySFedit. If not, see <http://www.gnu.org/licenses/>.
+
+"""
+This module tests the Byte class of the psflib.
+"""
+
 import unittest
 import psflib
 
