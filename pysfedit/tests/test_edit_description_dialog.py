ef200c6b54a382391bbccf6fd6213133db2a4962
Trumen <user@domain.tld>
2018-03-26 11:37:42 +0200

    Finished tests for the EditUnicodeDescriptionDialog

--- pysfedit/tests/test_edit_description_dialog.py
+++ pysfedit/tests/test_edit_description_dialog.py
@@ -30,13 +30,13 @@ class EditDescriptionDialogTest(unittest.TestCase):
 		desc.add_unicode_value(UnicodeValue(0x41))
 		desc.add_sequence(
             UnicodeSequence([
-                UnicodeValue(0x30a),
-                UnicodeValue(0x41)
+                UnicodeValue(0x41),
+                UnicodeValue(0x30a)
             ])
         )
 		self.dialog = EditUnicodeDescriptionDialog(
 			MockedParent(), desc, self.context)
-		
+	
 	def test_adding_values(self):
 		self.dialog.btn_add.clicked()
 		self.dialog.entry_unicode.set_text("a")
@@ -45,9 +45,7 @@ class EditDescriptionDialogTest(unittest.TestCase):
 		
 		self.assertIn(0x41, self.description.codepoints)
 		self.assertIn(ord('a'), self.description.codepoints)
-		self.assertIn([0x30a, 0x41],
-            [[int(v) for v in seq.values]
-                for seq in self.description.sequences])
+		self.assertIn([0x41, 0x30a], self.description.seq_codepoints)
 
 	def test_removing_values(self):
 		first_row = self.dialog.lb_descriptions.get_row_at_index(0)
@@ -56,8 +54,40 @@ class EditDescriptionDialogTest(unittest.TestCase):
 		self.dialog.response(Gtk.ResponseType.OK)
 		
 		self.assertEqual(self.description.unicode_values, [])
-		self.assertIn([0x30a, 0x41], self.description.seq_codepoints)
+		self.assertIn([0x41, 0x30a], self.description.seq_codepoints)
 
+	def test_editing_value(self):
+		first_row = self.dialog.lb_descriptions.get_row_at_index(0)
+		self.dialog.lb_descriptions.select_row(first_row)
+		self.dialog.entry_unicode.set_text("B")
+		self.dialog.btn_save.clicked()
+		self.dialog.response(Gtk.ResponseType.OK)
+
+		self.assertIn(0x42, self.description.codepoints)
+		self.assertIn([0x41, 0x30a], self.description.seq_codepoints)
+		
+	def test_editing_sequence(self):
+		self.context.allow_sequences()
+		
+		second_row = self.dialog.lb_descriptions.get_row_at_index(2)
+		self.dialog.lb_descriptions.select_row(second_row)
+		self.dialog.nb_editor.set_current_page(self.dialog.PAGE_VALUES)
+		self.dialog.entry_values.set_text(r"\u0042, \u030A")
+		self.dialog.btn_save.clicked()
+		self.dialog.response(Gtk.ResponseType.OK)
+
+		self.assertIn(0x41, self.description.codepoints)
+		self.assertIn([0x42, 0x30a], self.description.seq_codepoints)
+		
+	def test_abort_editing(self):
+		first_row = self.dialog.lb_descriptions.get_row_at_index(0)
+		self.dialog.lb_descriptions.select_row(first_row)
+		self.dialog.entry_unicode.set_text("B")
+		self.dialog.btn_save.clicked()
+		self.dialog.response(Gtk.ResponseType.CANCEL)
+
+		self.assertIn(0x41, self.description.codepoints)
+		self.assertIn([0x41, 0x30a], self.description.seq_codepoints)
 
 	def tearDown(self):
 		self.dialog.destroy()
